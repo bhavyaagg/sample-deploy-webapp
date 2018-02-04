d13e434617969900bafa4b5aa238ea46ec334c2b
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-02-04 16:39:56 +0530

    Connect DB
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 const PORT = process.env.PORT || 4000;
 
-exports = module.exports = {PORT}
+const DATABASE_URL = process.env.DATABASE_URL ||
+  "postgres://testuser:testpass@localhost:5432/testdb"
+
+exports = module.exports = {PORT, DATABASE_URL}
