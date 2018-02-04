79cb760bcb04cbc607f1ee4dd889bbe31d260057
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-02-04 16:18:05 +0530

    Update
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
-app.get('/hello', (r, s) => s.send("Hello"));
+app.get('/hello', (r, s) => s.send("Hello There"));
 
 app.listen(config.PORT, () => {
   console.log("Listening on port " + config.PORT);
